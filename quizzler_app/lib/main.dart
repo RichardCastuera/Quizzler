49f2c599e33f80b84d4cc96565361de3f5974d16
unknown <richardcastuera16@gmail.com>
2023-06-26 15:53:29 +0800

    feat(quizzler_app): Encapsulated the code

--- quizzler_app/lib/main.dart
+++ quizzler_app/lib/main.dart
@@ -38,8 +38,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  int questionNumber = 0;
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -52,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizBrain.questionBank[questionNumber].questionText!,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,13 +70,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true
-                bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer!;
+                bool correctAnswer = quizBrain.getCorrectAnswer();
                 (correctAnswer == true)
                     ? print('User got it right')
                     : print('User got it wrong');
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
               },
               child: const Text(
@@ -101,13 +98,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false
-                bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer!;
+                bool correctAnswer = quizBrain.getCorrectAnswer();
                 (correctAnswer == false)
                     ? print('User got it right')
                     : print('User got it wrong');
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
               },
               child: const Text(
